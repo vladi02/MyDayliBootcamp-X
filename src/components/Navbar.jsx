8955b4c3c4def80a7359869b99511fb989e3ac22
Ronaldoa1698 <ronaldo.delgado@unas.edu.pe>
2022-07-25 17:53:22 -0500

    se agrego el modal delete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,36 @@ function Navbar() {
   return (
     <div>
       <div className="header-wrapper">
-      <header className="header">
-        <nav className="nav">
-          <ul className="nav-list">
-            <li className="nav-item">
-              <a className="nav-link active" href="#"> All </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#"> Me </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="header-logo">
-          <a className="logo-link" href="index.html">
-            <img className="logo-img" src="./src/assets/logo.svg" alt="bootcamp logo" />
-            <h2 className="logo-text">My Daily Bootcamp</h2>
-          </a>
-        </div>
-      </header>
+        <header className="header">
+          <nav className="nav">
+            <ul className="nav-list">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  {" "}
+                  All{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  {" "}
+                  Me{" "}
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="header-logo">
+            <a className="logo-link" href="index.html">
+              <img
+                className="logo-img"
+                src="./src/assets/logo.svg"
+                alt="bootcamp logo"
+              />
+              <h2 className="logo-text">My Daily Bootcamp</h2>
+            </a>
+          </div>
+        </header>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 export default Navbar;
